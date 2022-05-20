9dbbc86e4eedaedae1f862fa25cba770bf56799b
Mykola Ostroukh <koluchiy.nick@gmail.com>
2022-05-20 10:13:08 +0000

    add new module Library Management

---
+++ library_app/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+    'summary': "Manage library catalog and book lending.",
+    'author': "Mikola Ostroukh",
+    'license': "AGPL-3"
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'application': True,
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
