6aa7a798315f55b50492891bd3601f6a054e9e11
Mykola Ostroukh <koluchiy.nick@gmail.com>
2022-06-01 10:20:30 +0000

    Chapter 6

---
+++ library_app/models/res_partner.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+    
+    published_book_ids = fields.One2many("library.book", "publisher_id", string="Published Books")
+    
